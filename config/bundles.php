e0130b8ac491e52eb0e85a1e9db151adaaffd00a
unknown <rhaveri19@epoka.edu.al>
2022-09-20 14:12:28 +0200

    Adding Twig and its beautiful recipe

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,7 @@
 <?php
-
+//A bundle in Symfony is basically a plugin,so if you install a new bundle into your app that gives u new Symfony features
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
